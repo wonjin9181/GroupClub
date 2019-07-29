ec0feb5ef212097f0c985b332fbd15c570f60567
WonJin <wonjin9181@gmail.com>
2019-07-29 13:04:45 -0700

    init backend created,  This api routes and models were created and also the database was created

---
+++ routes/post-api-routes.js
@@ -0,0 +1,47 @@
+
+var db = require("../models");
+
+module.exports = function(app) {
+
+
+  app.get("/api/posts/:id", function(req, res) {
+
+    db.Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Author]
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+
+  app.post("/api/posts", function(req, res) {
+    db.Post.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  app.delete("/api/posts/:id", function(req, res) {
+    db.Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  app.put("/api/posts", function(req, res) {
+    db.Post.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+};
