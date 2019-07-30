9cd6c4aefa8e0ff18ce95436f8c787ade53e3d66
WonJin <wonjin9181@gmail.com>
2019-07-30 14:53:26 -0700

    javascript search bar working

--- routes/clubs-api-routes.js
+++ routes/clubs-api-routes.js
@@ -1,46 +1,60 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/clubs", function(req, res) {
-    db.Clubs.findAll({})
-    .then(function(dbClubs) {
-   
-      res.json(dbClubs);
-      
-    });
-  });
-
-
-  app.get("/api/clubs/:name", function(req, res) {
-
-    db.Clubs.findAll({
-      where:{
-        clubName: req.params.name
+  app.get("/api/clubs", function (req, res) {
+    console.log(req.query)
+    var value = req.query.search
+    var expression = {}
+    console.log(value)
+    
+    if(value){
+      expression ={
+        where:{
+          clubName: value
+        }
       }
-    })
-    .then(function(dbClubs) {
-      res.json(dbClubs);
-    });
+    }
+
+    db.Clubs.findAll(expression)
+      .then(function (dbClubs) {
+
+        res.json(dbClubs);
+
+      });
   });
+
+
+  // app.get("/api/clubs/:name", function (req, res) {
+  //   console.log("This: " + req.params.name)
+  //   db.Clubs.findAll({
+  //     where: {
+  //       clubName: req.params.name
+  //     }
+  //   })
+  //     .then(function (dbClubs) {
+  //       console.log(dbClubs)
+  //       res.json(dbClubs);
+  //     });
+  // });
 
 
 
   // Create a new example
-  app.post("/api/clubs", function(req, res) {
+  app.post("/api/clubs", function (req, res) {
     db.Clubs.create(req.body)
-    .then(function(dbClubs) {
-      res.json(dbClubs);
-    });
+      .then(function (dbClubs) {
+        res.json(dbClubs);
+      });
   });
 
 
-  
+
   // Delete an example by id
-  app.delete("/api/clubs/:id", function(req, res) {
+  app.delete("/api/clubs/:id", function (req, res) {
     db.clubs.destroy({ where: { id: req.params.id } })
-    .then(function(dbClubs) {
-      res.json(dbClubs);
-    });
+      .then(function (dbClubs) {
+        res.json(dbClubs);
+      });
   });
 };
