f905af42761f0a7a6a2431aec98828cdfb87754f
sgidan <sarahgidan@gmail.com>
2019-08-01 10:47:44 -0700

    event.preventDefault for club submit btn

--- public/js/makeClub.js
+++ public/js/makeClub.js
@@ -1,32 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$("#submit").on("click", function(){
+    $("#submit").on("click", function () {
+        event.preventDefault();
 
-    
-    var clubName = $("#clubName").val().trim();
-    var clubMaker = $("#clubMaker").val().trim();
-    var clubLocation = $("#clubLocation").val().trim();
-    var clubDescription = $("#clubDescription").val().trim();
-    
-
-    var newClub = {
-        clubName,
-        clubMaker,
-        clubLocation,
-        clubDescription
-    }
-
-    console.log(newClub)
+        var clubName = $("#clubName").val().trim();
+        var clubMaker = $("#clubMaker").val().trim();
+        var clubLocation = $("#clubLocation").val().trim();
+        var clubDescription = $("#clubDescription").val().trim();
 
 
-    $.ajax({
-        type: "POST",
-        url: "api/clubs",
-        data: newClub
-    }).then(function(data){
-        window.location.href = "/"
+        var newClub = {
+            clubName,
+            clubMaker,
+            clubLocation,
+            clubDescription
+        }
+
+        console.log(newClub)
+
+
+        $.ajax({
+            type: "POST",
+            url: "/api/clubs",
+            data: newClub
+
+        }).then(function (data) {
+
+            window.location.pathname = "/";
+        })
     })
-})
 
 
 
