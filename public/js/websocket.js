dd5251bdf60081904cdaf05a41467a9d175f8b89
WonJin <wonjin9181@gmail.com>
2019-08-02 15:31:03 -0700

    env

---
+++ public/js/websocket.js
@@ -0,0 +1,42 @@
+$(function () {
+    const yourFunctionObject = {
+        myFunc: function (payload) {
+            $('.someElementClass').text(playload.message);
+        }
+    }
+  // if user is running mozilla then use it's built-in WebSocket
+  window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+  var connection = new WebSocket('ws://127.0.0.1:1337');
+
+  connection.onopen = function () {
+    // connection is opened and ready to use
+  };
+
+  connection.onerror = function (error) {
+    // an error occurred when sending/receiving data
+  };
+
+  connection.onmessage = function (message) {
+    message = {
+        data: {
+            function: 'setText',
+            payload: {
+                message: 'Some message.'
+            }
+        }
+    }
+
+    yourFunctionObject.myFunc(payload.message);
+
+
+
+    try {
+      var json = JSON.parse(message.data);
+    } catch (e) {
+        yourFunctionObject[json.function](json.payload);
+      return;
+    }
+    // handle incoming message
+  };
+});
