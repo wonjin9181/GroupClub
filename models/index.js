8c0b8a11530a8f09360636961120dcec66e5a36f
Surge Ali <surgeali@gmail.com>
2019-08-02 15:11:05 -0700

    env file setup

--- models/index.js
+++ models/index.js
@@ -1,20 +1,23 @@
 "use strict";
-
+require("dotenv").config();
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
+
 var env = process.env.NODE_ENV || "development";
+
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
+  let password = process.env.DB_PASSWORD;
   var sequelize = new Sequelize(
     config.database,
     config.username,
-    config.password,
+    password,
     config
   );
 }
