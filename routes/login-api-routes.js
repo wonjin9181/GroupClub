1a18adf8db3a76320a70ed231a7484f5f245aedd
WonJin <wonjin9181@gmail.com>
2019-08-02 10:14:33 -0700

    login page

--- routes/login-api-routes.js
+++ routes/login-api-routes.js
@@ -3,34 +3,34 @@ db = require("../models");
 module.exports = function(app) {
 
     app.get("/api/users",function(req, res){
-
-        db.Users.findAll({})
+        
+        var value = req.query.username
+        var expression = {}
+        console.log(value)
+        if (value){
+            expression = {
+                where: {
+                    username: value
+                }
+            }
+        }
+        db.Users.findAll(expression)
         .then(function(data){
             res.json(data);
         })
     })
 
     app.post("/api/users", function(req, res){
-        db.Users.create(req.body)
+        db.Users.create({
+            username: req.body.username,
+            password: req.body.password
+        })
         .then(function(data){
             res.json(data);
         })
     })
 
-    app.get("/api/users/password", function(req, res){
-        db.Users.findAll({
-            where:{
-                username : req.body.username,
-                password :req.body.password
-            }
-        })
-        .then(function(data){
-            res.json(data)
-            res.status(200)
-        })
-    })
-    
-
+ 
 
 
 }
